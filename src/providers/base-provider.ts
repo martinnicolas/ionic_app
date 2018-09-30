0378299cd5388ad4c8c91b7e5597c7d8c4a725d2
martinnicolas <ttincho77@gmail.com>
2018-09-30 17:18:39 -0300

    Agrego URL a backend_api en produccion

--- src/providers/base-provider.ts
+++ src/providers/base-provider.ts
@@ -1,4 +1,5 @@
 export abstract class BaseProvider{
     //protected baseURL = 'http://localhost:8084/backend'; // URL to web api Java
-    protected baseURL = 'http://192.168.1.129:3000'; // URL to web api Rails
+    //protected baseURL = 'http://192.168.1.129:3000'; // URL to web api Rails
+    protected baseURL = 'http://personasbackend.herokuapp.com'; // URL to web api Rails - Produccion
 }
